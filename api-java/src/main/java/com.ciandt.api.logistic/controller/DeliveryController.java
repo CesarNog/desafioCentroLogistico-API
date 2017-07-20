c6784a8143155ad9eac0c0b802aedd6795c79802
Cesar Augusto Nogueira <cesarnogueira1210@gmail.com>
2017-07-20 12:05:31 -0300

    Update DeliveryController.java

--- api-java/src/main/java/com.ciandt.api.logistic/controller/DeliveryController.java
+++ api-java/src/main/java/com.ciandt.api.logistic/controller/DeliveryController.java
@@ -4,8 +4,10 @@ import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import com.ciandt.api.logistic.Delivery;
+import com.ciandt.api.logistic.DeliveryStep;
 
 @RestController
 public class DeliveryController {
@@ -13,13 +15,12 @@ public class DeliveryController {
     private final AtomicLong counter = new AtomicLong();
 
     @RequestMapping("/delivery")
-    public Delivery delivery() {
-        return new Delivery(counter.incrementAndGet(),
-                            String.format(template));
+    public Delivery delivery(@RequestParam("vehicle") String vehicle, @RequestParam("deliveryId") int deliveryId, @RequestParam("packages") String packages) {
+        return new Delivery(vehicle, deliveryId, packages);
     }
 
-    @RequestMapping("/delivery/{deliveryId}/step")
-    public Delivery deliveryStep(@RequestParam(value="deliveryId") long deliveryId) {
-        return new Delivery(counter.incrementAndGet(), deliveryId);
+    @RequestMapping("/delivery/{deliveryId}/step", method = RequestMethod.GET)
+    public Delivery deliveryStep(@PathVariable int deliveryId) {
+        return new DeliveryStep(counter.incrementAndGet(), deliveryId, packages);
     }
 }
